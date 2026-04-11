873145fcb9e0b7e7b31c2e9c695ba67cc6b14322
adarshs-ui <adarsh.s@upgradsot.com>
2026-04-11 20:34:12 +0530

    changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { AppToaster } from "@/components/ui/app-toaster";
 import { Geist_Mono, Plus_Jakarta_Sans } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const plusJakarta = Plus_Jakarta_Sans({
@@ -35,6 +36,20 @@ export default function RootLayout({
       lang="en"
       className={`${plusJakarta.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-C0P6ZXJMQP"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-C0P6ZXJMQP');
+          `}
+        </Script>
+      </head>
       <body className="min-h-full flex flex-col font-sans">
         {children}
         <AppToaster />
